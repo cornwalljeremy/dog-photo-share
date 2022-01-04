14ad82d48549439629ef5292aaa9dc3f9b8c79e1
Jeremy Cornwall <cornwall.jeremy@gmail.com>
2022-01-03 20:02:31 -0700

    connected backend and front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Storage } from "aws-amplify";
 
-function App() {
+export default function App() {
+  const [images, setImages] = useState([])
+  useEffect(() => {
+    fetchImages()
+  }, []);
+  async function fetchImages() {
+    let imageKeys = await Storage.list('')
+    console.log('imageKeys 2: ', imageKeys)
+    imageKeys = await Promise.all(imageKeys.map(async k => {
+        const signedUrl = await Storage.get(k.key)
+        return signedUrl
+    }))
+    console.log('imageKeys 2: ', imageKeys)
+    setImages(imageKeys)
+
+
+  }
+
+  async function onChange(e) {
+    const file = e.target.files[0];
+    const result = await Storage.put(file.name, file)
+    console.log("result: ", result)
+    fetchImages();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Storage Example</h1>
+      <input type='file' onChange={onChange} />
+      {images.map((image) => (
+        <img
+          src={image}
+          key={image}
+          style={{ width: 300, height: 200, marginBottom: 10 }}
+        />
+      ))}
     </div>
   );
 }
-
-export default App;
