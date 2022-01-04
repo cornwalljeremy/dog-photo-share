07fba99a42535f2259a874300d7e5e57f48e7c7b
Jeremy Cornwall <cornwall.jeremy@gmail.com>
2022-01-03 19:27:51 -0700

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "dogphotoshare45da0b2d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "dogphoto": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
